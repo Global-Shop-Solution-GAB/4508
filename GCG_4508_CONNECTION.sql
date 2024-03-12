bd9d70d07c46c964d011c7f2e8ca643a3683d080
Service Web <sw@gssmail.com>
2024-03-12 09:03:40 -0500

    Commit changes in C:\Websites\Git\emarlow\4508\GCG_4508_CONNECTION.sql

---
+++ GCG_4508_CONNECTION.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_4508_CONNECTION
+ * ========================================== */
+CREATE TABLE "GCG_4508_CONNECTION"(
+ "NUM" CHAR(1),
+ "USER_NAME" CHAR(50),
+ "PASS" CHAR(50),
+ "SECTOK" CHAR(50));
+
+
