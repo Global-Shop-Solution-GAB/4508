d06da9148432b843fbf57d5aebf0c3ae0c291617
Service Web <sw@gssmail.com>
2024-03-12 09:03:40 -0500

    Commit changes in C:\Websites\Git\emarlow\4508\GCG_4508_Contact.sql

---
+++ GCG_4508_Contact.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_4508_Contact
+ * ========================================== */
+CREATE TABLE "GCG_4508_Contact"(
+ "LAST_CONTACT" VARCHAR(50),
+ "LAST_DATE" VARCHAR(30),
+ "column0" VARCHAR(50),
+ "column1" VARCHAR(30));
+
+
