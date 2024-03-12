8b32cdb835e3b81ea3c80bce0bd5f65b206116ca
Service Web <sw@gssmail.com>
2024-03-12 09:03:40 -0500

    Commit changes in C:\Websites\Git\emarlow\4508\GCG_4508_TRACKING_NO.sql

---
+++ GCG_4508_TRACKING_NO.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_4508_TRACKING_NO
+ * ========================================== */
+CREATE TABLE "GCG_4508_TRACKING_NO"(
+ "ORDER_NO" CHAR(7),
+ "ORDER_SUFFIX" CHAR(4),
+ "SF_ID" CHAR(20));
+
+
